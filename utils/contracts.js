3d2b6e321d0b7d51e04d5171137977e5785f6807
Hafiz Muhammad Abdullah <72753811+hafizabdullah510@users.noreply.github.com>
2024-03-31 00:56:19 +0500

    path commit 2

--- utils/contracts.js
+++ utils/contracts.js
@@ -2,16 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import { AlchemyProvider, ethers } from "ethers";
 import fs from "fs-extra";
-
-export const getContract = async (abiPath, addressPath) => {
-  console.log(process.cwd());
+export const getContract = async () => {
   const provider = new AlchemyProvider("sepolia", process.env.ALCHEMY_API_KEY);
   const privateKey =
     "88651019206dfec02cd69d39a5a4838bb41b1c43de91fedc02e8b85796e0bfda";
 
   const wallet = new ethers.Wallet(privateKey, provider);
   const contractAddress = await fs.readFile(
-    "CardDetailsContractAddress.txt",
+    process.cwd() + "/CardDetailsContractAddress.txt",
     "utf-8"
   );
   const abi = await fs.readFile(
